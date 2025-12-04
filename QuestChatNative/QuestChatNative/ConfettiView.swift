b16e9be35d840097d791bdacc1c8e45dfd81acc4
Statusnone420 <anthonyga88@icloud.com>
2025-12-04 17:54:10 -0500

    Update ConfettiView.swift

--- QuestChatNative/QuestChatNative/ConfettiView.swift
+++ QuestChatNative/QuestChatNative/ConfettiView.swift
@@ -10,7 +10,7 @@ struct ConfettiView: UIViewRepresentable {
         emitter.emitterPosition = CGPoint(x: UIScreen.main.bounds.width / 2, y: -10)
         emitter.emitterShape = .line
         emitter.emitterSize = CGSize(width: UIScreen.main.bounds.width, height: 2)
-        emitter.renderMode = .unordered   // keep it simple
+        emitter.renderMode = .backToFront
 
         emitter.emitterCells = makeEmitterCells()
         view.layer.addSublayer(emitter)
@@ -31,39 +31,75 @@ struct ConfettiView: UIViewRepresentable {
             UIColor(red: 0.60, green: 1.00, blue: 0.60, alpha: 1.0)  // green
         ]
 
-        return colors.map { color in
-            let cell = CAEmitterCell()
-            cell.birthRate = 12
-            cell.lifetime = 4.0
+        var cells: [CAEmitterCell] = []
 
-            cell.velocity = 220
-            cell.velocityRange = 80
-            cell.yAcceleration = 120
+        for color in colors {
+            // Round confetti (small dots)
+            if let circleImage = makeCircleImage(color: color)?.cgImage {
+                let cell = baseCell()
+                cell.contents = circleImage
+                cell.birthRate = 10
+                cell.scale = 0.6
+                cell.scaleRange = 0.3
+                cell.spin = 3
+                cell.spinRange = 1.5
+                cells.append(cell)
+            }
 
-            cell.emissionLongitude = .pi
-            cell.emissionRange = .pi / 4
-
-            cell.spin = 3.5
-            cell.spinRange = 1.5
-
-            cell.scale = 0.9
-            cell.scaleRange = 0.4
-
-            // draw a colored circle image for THIS cell
-            cell.contents = makeCircleImage(color: color)?.cgImage
-
-            return cell
+            // Long strips (classic confetti look)
+            if let stripImage = makeStripImage(color: color)?.cgImage {
+                let cell = baseCell()
+                cell.contents = stripImage
+                cell.birthRate = 6
+                cell.scale = 1.0
+                cell.scaleRange = 0.4
+                cell.spin = 6
+                cell.spinRange = 3
+                cell.xAcceleration = CGFloat.random(in: -40...40)
+                cells.append(cell)
+            }
         }
+
+        return cells
+    }
+
+    private func baseCell() -> CAEmitterCell {
+        let cell = CAEmitterCell()
+        cell.lifetime = 4.0
+
+        cell.velocity = 240
+        cell.velocityRange = 100
+        cell.yAcceleration = 130
+
+        cell.emissionLongitude = .pi
+        cell.emissionRange = .pi / 3
+
+        cell.alphaSpeed = -0.3   // fade out as they fall
+        return cell
     }
 
     private func makeCircleImage(color: UIColor) -> UIImage? {
-        let size: CGFloat = 14
+        let size: CGFloat = 10
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: size, height: size))
 
         return renderer.image { context in
             let rect = CGRect(x: 0, y: 0, width: size, height: size)
             color.setFill()
             context.cgContext.fillEllipse(in: rect)
+        }
+    }
+
+    private func makeStripImage(color: UIColor) -> UIImage? {
+        // Tall skinny rectangle
+        let width: CGFloat = 6
+        let height: CGFloat = 14
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+
+        return renderer.image { context in
+            let rect = CGRect(x: 0, y: 0, width: width, height: height)
+            color.setFill()
+            context.cgContext.fill(rect)
+            context.cgContext.setBlendMode(.normal)
         }
     }
 }
